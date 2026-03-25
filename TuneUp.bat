a2418720f160122a443cddcdaef8432b40e5b36b
Felixplored <felixplored@gmail.com>
2026-03-25 21:19:07 +0100

    Add files via upload

--- TuneUp.bat
+++ TuneUp.bat
@@ -1,5 +1,5 @@
 @echo off
-title System TuneUp by Felixplored v. 1.0
+title System TuneUp by Felixplored v. 1.1
 echo on
 rem Bitte ALLE Haken setzen und mit: "OK" bestaetigen.
 @echo off
@@ -14,10 +14,20 @@ dism /Online /Cleanup-Image /StartComponentCleanup
 dism /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 dism /Online /Cleanup-Image /spsuperseded
 ipconfig /flushdns
-defrag C:
+defrag C: /u
 chkdsk
 wsreset
 taskkill /IM WinStore.App.exe /f /t
+vssadmin delete shadows /all /quiet
+wmic /namespace:\\root\default path SystemRestore call Disable C:\
+del %localappdata%\NVIDIA\GLCache /f /q /s
+for /d %%a in ("%localappdata%\NVIDIA\GLCache\*.*") do rd /q /s "%%a"
+del %localappdata%\NVIDIA\OptixCache /f /q /s
+for /d %%a in ("%localappdata%\NVIDIA\OptixCache\*.*") do rd /q /s "%%a"
+del %localappdata%low\NVIDIA\PerDriverVersion\DXCache /f /q /s
+for /d %%a in ("%localappdata%low\NVIDIA\PerDriverVersion\DXCache\*.*") do rd /q /s "%%a"
+del %appdata%\NVIDIA\ComputeCache /f /q /s
+for /d %%a in ("%appdata%\NVIDIA\ComputeCache\*.*") do rd /q /s "%%a"
 del %temp% /f /q /s
 for /d %%a in ("%temp%\*.*") do rd /q /s "%%a"
 del C:\Windows\Temp /f /q /s
@@ -29,7 +39,15 @@ for /d %%a in ("C:\Windows\Prefetch\*.*") do rd /q /s "%%a"
 del C:\Windows\SoftwareDistribution\Download /f /q /s
 for /d %%a in ("C:\Windows\SoftwareDistribution\Download\*.*") do rd /q /s "%%a"
 echo on
-rem Bereinigung beendet!
+rem Bereinigung beendet! Ein Neustart wird zwingend Empfohlen.
 @echo off
-pause
+set auswahl=
+set /p auswahl="Moechten Sie das System Neustarten? (y/n)"
+if "%auswahl%" == "y" goto ja
+if "%auswahl%" == "n" goto nein
+:ja
+shutdown /r
+:nein
+exit
+
 
--- readme.txt
+++ readme.txt
@@ -1,7 +1,11 @@
-System TuneUp by Felixplored v. 1.0
+System TuneUp by Felixplored v. 1.1
 
 Versionshistorie:
-・ Versionshistorie erstellt (v. 1.0 / 22.11.2024)
+・ Fortschritt für die Defragmentierung wird jetzt angezeigt,
+  Die Systemwiederherstellung wird Bereinigt und Deaktiviert, 
+  Die NVIDIA Caches werden Bereinigt,
+  Neustart Funktion hinzugefügt. (v. 1.1 / 24.11.2024)
+・ Versionshistorie erstellt. (v. 1.0 / 22.11.2024)
 
 Diese Batch Datei führt eine Bereinigung des Computers durch mit Hauseigenen Windows Tools per Befehls-Skript.
 
@@ -13,7 +17,8 @@ Vorsicht:
 Durch Drücken einer beliebigen Taste wird das Programm weiter ausgeführt.
 Abbrechen erfolgt, wenn das Fenster geschlossen wird.
 Die Bereinigung kann je nach Computer länger andauern. 
-Die Bereinigung ist Abgeschlossen, wenn in der Kommandozeile: "Bereinigung beendet!" steht.
+Die Bereinigung ist Abgeschlossen, wenn in der Kommandozeile: "Bereinigung beendet! Ein Neustart wird zwingend Empfohlen." steht.
+Es muss am Ende eine Wahl zwecks Neustart getroffen werden. (y/n) - ENTER führt ebenfalls einen Neustart durch.
 
 Bekannte Fehler:
 Sollte das Befehls-Skript beim ersten Schritt hängen bleiben starte die: "TuneUp.bat" neu. 
