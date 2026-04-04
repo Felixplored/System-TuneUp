b698a1fa2841e0ceb5d6265ad87bddea4e72c685
Felixplored <felixplored@gmail.com>
2026-04-05 00:03:09 +0200

    Add files via upload

--- TuneUp.bat
+++ TuneUp.bat
@@ -7,7 +7,7 @@ if %errorlevel% neq 0 (
    exit
 )
 
-title System TuneUp by Felixplored v. 1.15
+title System TuneUp by Felixplored v. 1.16
 
 REM Windows Datenträgerbereinigung
 echo Ersteinrichtung: Bitte ALLE Haken setzen und mit: "OK" bestaetigen.
@@ -37,29 +37,51 @@ dism /Online /Cleanup-Image /SPSuperseded
 REM Deaktivierung des Features: "Recall"
 dism /Online /Disable-Feature /Featurename:Recall
 
-REM Systemwiederherstellungspunkte Löschen & Systemwiederherstellung Deaktivieren für Laufwerk C:
-vssadmin delete shadows /all /quiet
-wmic /namespace:\\root\default path SystemRestore call Disable C:\
-
-REM Ruhezustand deaktivieren
-powercfg -h off
-
-REM Dateisystemfehler prüfen & Defragmentierung von Laufwerk C: durchführen
+REM Dateisystemfehler prüfen & Defragmentierung durchführen für Laufwerk C:
 chkdsk C: /f
 defrag C: /h /u
 
+REM Systemwiederherstellungspunkte löschen & Systemwiederherstellung deaktivieren für Laufwerk C:
+cls
+echo [==                        5.0%%                            ]
+(
+vssadmin delete shadows /all /quiet
+wmic /namespace:\\root\default path SystemRestore call Disable C:\
+) >nul 2>&1
+
+REM Schnellstart & Ruhezustand deaktivieren
+cls
+echo [=====                     10.0%%                           ]
+(
+powercfg -h off
+) >nul 2>&1
+
 REM DNS Cache leeren
+cls
+echo [========                  15.0%%                           ]
+(
 ipconfig /flushdns
+) >nul 2>&1
 
 REM Microsoft Store Cache leeren & Programm schließen
+cls
+echo [==========                20.0%%                           ]
+(
 wsreset
 taskkill /IM WinStore.App.exe /f /t
+) >nul 2>&1
 
 REM Windows Event Logs leeren
-timeout /t 5 /nobreak >nul
+cls
+echo [=============             25.0%%                           ]
+(
 for /F "tokens=*" %%1 in ('wevtutil.exe el') DO wevtutil.exe cl "%%1"
+) >nul 2>&1
 
-REM Versteckte Windows Ordner Löschen
+REM Versteckte Windows Ordner löschen
+cls
+echo [================          30.0%%                           ]
+(
 rd C:\$GetCurrent /q /s
 rd C:\$SysReset /q /s
 rd C:\$Windows.~BT /q /s
@@ -68,12 +90,20 @@ rd C:\$WinREAgent /q /s
 rd C:\Config.Msi /q /s
 rd C:\Recovery /q /s
 rd C:\Windows.old /q /s
+) >nul 2>&1
 
 REM Perflogs Ordner leeren
+cls
+echo [==================        35.0%%                           ]
+(
 del C:\PerfLogs /f /q /s
 for /d %%a in ("C:\PerfLogs\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM AMD Cache leeren
+cls
+echo [=====================     40.0%%                           ]
+(
 del C:\.cache\AMD\DX9Cache /f /q /s
 for /d %%a in ("C:\.cache\AMD\DX9Cache\*.*") do rd /q /s "%%a"
 del %localappdata%\AMD\DX9Cache /f /q /s
@@ -90,8 +120,12 @@ del %localappdata%low\AMD\DxCache /f /q /s
 for /d %%a in ("%localappdata%low\AMD\DxCache\*.*") do rd /q /s "%%a"
 del %localappdata%low\AMD\DxcCache /f /q /s
 for /d %%a in ("%localappdata%low\AMD\DxcCache\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM NVIDIA Cache leeren
+cls
+echo [=======================   45.0%%                           ]
+(
 del %localappdata%\NVIDIA\DXCache /f /q /s
 for /d %%a in ("%localappdata%\NVIDIA\DXCache\*.*") do rd /q /s "%%a"
 del %localappdata%\NVIDIA\GLCache /f /q /s
@@ -104,22 +138,38 @@ del %localappdata%low\NVIDIA\PerDriverVersion\DXCache /f /q /s
 for /d %%a in ("%localappdata%low\NVIDIA\PerDriverVersion\DXCache\*.*") do rd /q /s "%%a"
 del %appdata%\NVIDIA\ComputeCache /f /q /s
 for /d %%a in ("%appdata%\NVIDIA\ComputeCache\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM D3DS Cache leeren
+cls
+echo [==========================50.0%%                           ]
+(
 del %localappdata%\D3DSCache /f /q /s
 for /d %%a in ("%localappdata%\D3DSCache\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Qt Shader Cache leeren
+cls
+echo [==========================55.0%%==                         ]
+(
 del %localappdata%\cache /f /q /s
 for /d %%a in ("%localappdata%\cache\*.*") do rd /q /s "%%a"
 del %localappdata%\fontconfig /f /q /s
 for /d %%a in ("%localappdata%\fontconfig\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Task Scheduler Cache leeren
+cls
+echo [==========================60.0%%=====                      ]
+(
 del C:\Windows\SchCache /f /q /s
 for /d %%a in ("C:\Windows\SchCache\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Windows Temps leeren
+cls
+echo [==========================65.0%%========                   ]
+(
 del %temp% /f /q /s
 for /d %%a in ("%temp%\*.*") do rd /q /s "%%a"
 del %localappdata%\SquirrelTemp /f /q /s
@@ -134,8 +184,12 @@ del C:\Windows\CbsTemp /f /q /s
 for /d %%a in ("C:\Windows\CbsTemp\*.*") do rd /q /s "%%a"
 del C:\Windows\SoftwareDistribution\Download /f /q /s
 for /d %%a in ("C:\Windows\SoftwareDistribution\Download\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Windows CrashDumps leeren
+cls
+echo [==========================70.0%%==========                 ]
+(
 del %localappdata%\CrashDumps /f /q /s
 for /d %%a in ("%localappdata%\CrashDumps\*.*") do rd /q /s "%%a"
 del C:\ProgramData\Microsoft\Windows\WER /f /q /s
@@ -144,16 +198,25 @@ del C:\Windows\LiveKernelReports /f /q /s
 for /d %%a in ("C:\Windows\LiveKernelReports\*.*") do rd /q /s "%%a"
 del C:\Windows\Minidump /f /q /s
 for /d %%a in ("C:\Windows\Minidump\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Windows Logs leeren
+cls
+echo [==========================75.0%%=============              ]
+(
 del C:\Windows\Logs /f /q /s
 for /d %%a in ("C:\Windows\Logs\*.*") do rd /q /s "%%a"
 del C:\Windows\debug /f /q /s
 for /d %%a in ("C:\Windows\debug\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Windows Prefetch leeren
+cls
+echo [==========================80.0%%================           ]
+(
 del C:\Windows\Prefetch /f /q /s
 for /d %%a in ("C:\Windows\Prefetch\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Explorer beenden & prüfen ob beendet
 taskkill /f /im explorer.exe >nul 2>&1
@@ -165,23 +228,36 @@ if not errorlevel 1 (
 )
 
 REM Icon Cache leeren
+cls
+echo [==========================85.0%%==================         ]
+(
 del %localappdata%\IconCache.db /a /f /q
+) >nul 2>&1
 
 REM Thumbnail Cache leeren
+cls
+echo [==========================90.0%%=====================      ]
+(
 del %localappdata%\Microsoft\Windows\Explorer /f /q /s
 for /d %%a in ("%localappdata%\Microsoft\Windows\Explorer\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Weitere Windows Caches leeren
+cls
+echo [==========================95.0%%=======================    ]
+(
 del %localappdata%\Microsoft\Windows\Caches /f /q /s
 for /d %%a in ("%localappdata%\Microsoft\Windows\Caches\*.*") do rd /q /s "%%a"
 del C:\ProgramData\Microsoft\Windows\Caches /f /q /s
 for /d %%a in ("C:\ProgramData\Microsoft\Windows\Caches\*.*") do rd /q /s "%%a"
+) >nul 2>&1
 
 REM Explorer starten
+cls
+echo [==========================100.0%%==========================]
 start explorer.exe
 
 REM Neustart Abfrage
-cls
 echo Bereinigung beendet! Ein Neustart wird zwingend empfohlen.
 set auswahl=
 set /p auswahl="Moechten Sie das System Neustarten? (y/n)"
--- readme.txt
+++ readme.txt
@@ -1,9 +1,10 @@
-System TuneUp by Felixplored v. 1.15
+System TuneUp by Felixplored v. 1.16
 
 Versionshistorie:
+v. 1.16 (05.04.2026) ・ Fortschrittsanzeige hinzugefügt.
 v. 1.15 (04.04.2026) ・ Der Ruhezustand wird nun deaktiviert, Der Explorer wird nun vor dem leeren der Windows Caches beendet und erneut ausgeführt,
                        Der Icon Cache in Local AppData wird nun Bereinigt, Der Ordner: "Microsoft\Windows\Caches" in ProgrammData wird nun Bereinigt,
-					   Der Qt Shader Cache wird nun Bereinigt, Windows CrashDumps werden nun Bereinigt, Weitere Logs, Caches & Temps werden nun Bereinigt.			   
+		               Der Qt Shader Cache wird nun Bereinigt, Windows CrashDumps werden nun Bereinigt, Weitere Logs, Caches & Temps werden nun Bereinigt.			   
 v. 1.14 (03.04.2026) ・ Admin-Check Funktion hinzugefügt damit das Batch immer als Administrator ausgeführt wird,
                        Der Befehl: "chkdsk" wird nun mit dem Parameter C: /f ausgeführt.
 v. 1.13 (01.04.2026) ・ Speicheroptimierung (Storage Sense) wird nun abgefragt.
